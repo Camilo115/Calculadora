1244677da8c43966ac721da112c91c42d1bdddf3
Camilo <cq_electrocolor@hotmail.com>
2020-06-29 17:49:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def suma(n1,n2):
 
 @app.route('/resta/<int:n1>/<int:n2>',methods=['GET'])
 def resta(n1,n2):
-	return '<h1>Bienvenido a la calculadora</h1><br> <h2>El resultado de la resta es: %s</h2>'%str(n1-n2)
+	return '<h1>Bienvenido a la calculadora</h1><br> <h2>El resultado de la resta es: %s</h2>' %str(n1-n2)
 
 @app.route('/multi/<int:n1>/<int:n2>',methods=['GET'])
 def multi(n1,n2):
